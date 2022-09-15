dc2a2d5ad205fd4c65410d16c9ca91e19e695f42
george@olp-europe.com <george>
2022-09-15 05:58:55 +0300

    Initialize banking lib

---
+++ errs/errs.go
@@ -0,0 +1,28 @@
+package errs
+
+import "net/http"
+
+type AppError struct {
+	Code    int    `json:",omitempty"`
+	Message string `json:"message"`
+}
+
+func NewUnexpectedError(message string) *AppError {
+	return &AppError{
+		Message: message,
+		Code:    http.StatusNotFound,
+	}
+}
+
+func NewValidationError(message string) *AppError {
+	return &AppError{
+		Message: message,
+		Code:    http.StatusUnprocessableEntity,
+	}
+}
+
+func (e AppError) AsMessage() *AppError {
+	return &AppError{
+		Message: e.Message,
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/georgegeorgiev0/banking-lib
+
+go 1.19
